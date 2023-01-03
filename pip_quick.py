3a823a0a61ed3f560e73e149d0560e107c5342c3
4rge <93495092+4rge@users.noreply.github.com>
2023-01-03 16:26:39 -0600

    Update pip_quick.py

--- pip_quick.py
+++ pip_quick.py
@@ -1,20 +1,22 @@
-import subprocess
-from os.path import exists
-
-
 def pip_quick():
-	if exists("/etc/apt/sources.list") == True :  subprocess.call(["sudo apt install pip"], shell=True)
-	else exists("/ect/pacman.d") == True : subprocess.call(["sudo pacman -S pip"], shell=True)
-	else print("OS not supported")
-	
-	commands = {'requests', 'qrcode', 'urllib3', 'bs4', 'youtube_dl', 'wget', 'wikipedia'}
+    if exists("/etc/apt/sources.list"):
+        subprocess.call(["sudo apt install pip"], shell=True)
+    elif exists("/ect/pacman.d"):
+        subprocess.call(["sudo pacman -S pip"], shell=True)
+    else:
+        print("OS not supported")
 
-	for call in commands:
-		subprocess.call(["pip install "+call], shell=True)
-	
-	py = {"python3-pyqt5", "python3-pyqt5.qtwebengine"}
+    commands = {'requests', 'qrcode', 'urllib3', 'bs4', 'youtube_dl', 'wget', 'wikipedia'}
 
-	for sudo in py:
-		if exists("/etc/apt/sources.list") == True :  subprocess.call(["sudo apt install "+sudo], shell=True)
-		else exists("/ect/pacman.d") == True : subprocess.call(["sudo pacman -S "+sudo], shell=True)
-		else print("OS not supported")
+    for call in commands:
+        subprocess.call(["pip install "+call], shell=True)
+        
+    py = ["python3-pyqt5", "python3-pyqt5.qtwebengine"]
+
+    for sudo in py:
+        if exists("/etc/apt/sources.list"):
+            subprocess.call(["sudo apt install "+sudo], shell=True)
+        elif exists("/ect/pacman.d"):
+            subprocess.call(["sudo pacman -S "+sudo], shell=True)
+        else:
+            print("OS not supported")
